3e019672c0567eddfefe57c3f0e347e5cad7890d
MalinAan <malinaandahl@gmail.com>
2016-01-16 11:50:56 +0100

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,22 @@
+
+def hi(name):
+    print('Hi there ' + name + "!")
+    print('How are you?')
+
+print("HELLO");
+if 3 > 2:
+    print('It works!')
+
+girls = ['Malin', 'Henrietta', 'Nora', 'Marte', 'Trygve']
+
+for name in girls:
+	hi(name)
+	print('Next girl')
+
+for i in range(1, 6):
+	print(i)
+
+
+#hi('Malin2')
+#hi('Henrietta2')
+
